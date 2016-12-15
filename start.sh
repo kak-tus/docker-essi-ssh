f5e8723352cac32a534a00a2e419bc49845022d4
Andrey Kuzmin <prepodam@mail.ru>
2016-12-15 21:40:42 +0300

    Clone repos fix.
    We non non-root, so create ssh keys in another home dir.

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 consul-template -once -template="/root/generate.sh.template:/root/generate.sh" \
   && chmod +x /root/generate.sh \
   && /root/generate.sh \
-  && consul-template -once -template="/root/config.template:/root/.ssh/config" \
-  && chmod 600 /root/.ssh/* \
+  && consul-template -once -template="/root/config.template:/home/www-data/.ssh/config" \
+  && chmod 600 /home/www-data/.ssh/* \
+  && chown -R www-data:www-data /home/www-data/.ssh \
   && /usr/local/bin/start_essi.sh
